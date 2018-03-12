9bc20e5b6630d5c7b457036028b38c54d03ef38c
Michael Fryer <michael.d.fryer@gmail.com>
2018-03-12 16:55:35 -0600

    initial commit. Setting up gradle

---
+++ App/src/main/java/App.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    /*
+     * (non-Javadoc)
+     * @see javafx.application.Application#start(javafx.stage.Stage)
+     */
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // I hate java
+        // ¯\_(ツ)_/¯
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
