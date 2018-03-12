701e0cfd6a005927d6c62f5a864ce3b8530c2e7b
Michael Fryer <michael.d.fryer@gmail.com>
2018-03-12 17:00:41 -0600

    updated AppTest to be named correctly

--- App/src/test/java/AppTest.java
+++ App/src/test/java/AppTest.java
@@ -5,7 +5,7 @@ public class AppTest {
     @Test
     public void testAppHasAGreeting() {
         App classUnderTest = new App();
-        assertNotNull("app should have a greeting",
+        assertNotNull("App should not be null",
             classUnderTest);
     }
 }
