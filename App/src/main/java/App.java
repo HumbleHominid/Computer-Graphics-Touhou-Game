9ba03d52ce4ba0f3763bab3826f06e7657ffae73
Michael Fryer <michael.d.fryer@gmail.com>
2018-03-12 19:17:14 -0600

    got the initial bones of a game loop set up. tickrate of 60 and unlimited fps

--- App/src/main/java/App.java
+++ App/src/main/java/App.java
@@ -1,18 +1,153 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
+import static com.jogamp.opengl.GL4.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.awt.TextRenderer;
 
-public class App extends Application {
-    /*
-     * (non-Javadoc)
-     * @see javafx.application.Application#start(javafx.stage.Stage)
-     */
-    @Override
-    public void start(Stage arg0) throws Exception {
-        // I hate java
-        // ¯\_(ツ)_/¯
+import java.awt.*;
+
+import javax.swing.*;
+
+import java.util.ArrayList;
+
+public class App extends JFrame implements GLEventListener {
+    // Game tick set to 60 ticks per second
+    private final double MS_PER_UPDATE = 1000 / 60;
+    private GLCanvas _myCanvas;
+    // Frame tracking stuff
+    // list of the past N render times. for getting fps values
+    private ArrayList<Double> _renderTimes = new ArrayList<Double>();
+    // the amount of the next frame we covered in our update method
+    private double _frameCovered = 0.0;
+
+    public App() {
+        // Set the window properties
+        setTitle("Crappy Touhou Clone dot EXE");
+        setSize(800, 600);
+
+        // Create new canvas
+        _myCanvas = new GLCanvas();
+
+        // Add canvas listeners
+        _myCanvas.addGLEventListener(this);
+
+        // Add my canvas to the pane and set it to visible
+        getContentPane().add(_myCanvas);
+        setVisible(true);
     }
 
+    /**
+     * Main game loop function. updates the game according to the tickrate set
+     * by MS_PER_UPDATE. Will render the game as fast as it can.
+     */
+    public void gameLoop() {
+        double current;
+        double elapsed;
+        double lag = 0.0;
+        double previous = System.currentTimeMillis();
+
+        while (true) {
+            current = System.currentTimeMillis();
+            elapsed = current - previous;
+
+            lag = lag + elapsed;
+            previous = current;
+
+            processInput();
+
+            // keep updating the game while we are running at slow fps
+            while (lag >= MS_PER_UPDATE) {
+                update();
+
+                lag = lag - MS_PER_UPDATE;
+            }
+
+            // Track the elapsed time for displaying fps purposes
+            _renderTimes.add(elapsed);
+
+            if (_renderTimes.size() > 10) {
+                _renderTimes.remove(0);
+            }
+
+            // set frameCovered. this is for extrapolating rendering
+            _frameCovered = lag / MS_PER_UPDATE;
+
+            _myCanvas.display();
+        }
+    }
+
+    public void processInput() {
+        // TODO
+    }
+
+    public void update() {
+        // TODO
+    }
+
+    /* ************************
+     * GLEventListener Handlers
+     * ***********************/
+    /*
+     * (non-Javadoc)
+     * @see com.jogamp.opengl.GLEventListener#display(com.jogamp.opengl.GLAutoDrawable)
+     */
+    @Override
+    public void display(GLAutoDrawable glAD) {
+        GL4 gl = (GL4) GLContext.getCurrentGL();
+
+        // Clearing the canvas is important
+        gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // print some text
+        TextRenderer textRenderer = new TextRenderer(new Font("Verdana", Font.BOLD, 18));
+
+        textRenderer.beginRendering(glAD.getSurfaceWidth(), glAD.getSurfaceHeight());
+        textRenderer.setColor(Color.YELLOW);
+        textRenderer.setSmoothing(true);
+
+        // Get the sum of the render times in the list
+        double totalTime = 0;
+
+        for (Double d : _renderTimes) {
+            totalTime = totalTime + d;
+        }
+
+        // Display the average of the render times
+        textRenderer.draw(String.format("%.01f ms", totalTime / _renderTimes.size()), 0, 0);
+        textRenderer.endRendering();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.jogamp.opengl.GLEventListener#dispose(com.jogamp.opengl.GLAutoDrawable)
+     */
+    @Override
+    public void dispose(GLAutoDrawable arg0) {
+        // TODO
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.jogamp.opengl.GLEventListener#init(com.jogamp.opengl.GLAutoDrawable)
+     */
+    @Override
+    public void init(GLAutoDrawable arg0) {
+        // TODO
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.jogamp.opengl.GLEventListener#reshape(com.jogamp.opengl.GLAutoDrawable, int, int, int, int)
+     */
+    @Override
+    public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
+        // TODO
+    }
+
+    /*
+     * main
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        App app = new App();
+        app.gameLoop();
     }
 }
--- App/src/test/java/AppTest.java
+++ App/src/test/java/AppTest.java
@@ -4,8 +4,7 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test
     public void testAppHasAGreeting() {
-        App classUnderTest = new App();
         assertNotNull("App should not be null",
-            classUnderTest);
+            new App());
     }
 }
