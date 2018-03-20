8401d8870c377f948fdb0ab9e2dd48e521dd6669
Michael Fryer <michael.d.fryer@gmail.com>
2018-03-20 11:59:36 -0600

    added a hard-coded build script for handling combining use-cases. will revise to a more general script later

---
+++ Docs/build.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// If there is a previos doc, delete it
+if (fs.existsSync('srs.md')) {
+    fs.unlinkSync('srs.md');
+}
+
+fs.writeFileSync('srs.md', '');
+
+// Hard coded search through `Use_Cases` dir for now
+let caseData = JSON.parse(fs.readFileSync('Use_Cases/.build'));
+caseData.forEach((useCase) => {
+    let fileData = fs.readFileSync(`Use_Cases/${useCase.file}`);
+
+    fs.appendFileSync('srs.md', `### ${useCase.name}\n${fileData}`);
+});
